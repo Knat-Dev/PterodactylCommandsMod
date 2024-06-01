07906179a9ed2faf8c3eb08c1e07fb2a0e8efb72
Dor Peled <knat.dev.93@gmail.com>
2024-06-01 16:41:26 +0300

    validate connection to server api

---
+++ src/main/java/com/dorpeled/pterodactylcommandsmod/network/RestClient.java
@@ -0,0 +1,87 @@
+package com.dorpeled.pterodactylcommandsmod.network;
+
+import com.dorpeled.pterodactylcommandsmod.config.PterodactylCommandsConfig;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class RestClient {
+    private static HttpClient httpClient;
+
+    private RestClient() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static HttpClient getHttpClient() {
+        if (httpClient == null) {
+            httpClient = HttpClient.newBuilder()
+                    .version(HttpClient.Version.HTTP_2)
+                    .connectTimeout(Duration.ofSeconds(10))
+                    .build();
+        }
+        return httpClient;
+    }
+
+    public static HttpResponse<String> sendGetRequest(String url) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot")
+                .header("Content-Type", "application/json")
+                .setHeader("Authorization", "Bearer " + PterodactylCommandsConfig.API_KEY.get())
+                .build();
+
+        return getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> sendPostRequest(String url, String body) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .uri(URI.create(url))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot")
+                .setHeader("Authorization", "Bearer " + PterodactylCommandsConfig.API_KEY.get())
+                .header("Content-Type", "application/json")
+                .build();
+
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> sendPutRequest(String url, String body) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .PUT(HttpRequest.BodyPublishers.ofString(body))
+                .uri(URI.create(url))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot")
+                .setHeader("Authorization", "Bearer " + PterodactylCommandsConfig.API_KEY.get())
+                .header("Content-Type", "application/json")
+                .build();
+
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> sendDeleteRequest(String url) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .DELETE()
+                .uri(URI.create(url))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot")
+                .setHeader("Authorization", "Bearer " + PterodactylCommandsConfig.API_KEY.get())
+                .header("Content-Type", "application/json")
+                .build();
+
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> sendPatchRequest(String url, String body) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .method("PATCH", HttpRequest.BodyPublishers.ofString(body))
+                .uri(URI.create(url))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot")
+                .setHeader("Authorization", "Bearer " + PterodactylCommandsConfig.API_KEY.get())
+                .header("Content-Type", "application/json")
+                .build();
+
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
