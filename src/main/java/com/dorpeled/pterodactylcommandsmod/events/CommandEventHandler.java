ef1739fc25965f16e85cd4ef2358708c3672f29c
Dor Peled <knat.dev.93@gmail.com>
2024-06-01 14:27:19 +0300

    Config loading successfully

---
+++ src/main/java/com/dorpeled/pterodactylcommandsmod/events/CommandEventHandler.java
@@ -0,0 +1,19 @@
+package com.dorpeled.pterodactylcommandsmod.events;
+
+import com.dorpeled.pterodactylcommandsmod.commands.HelpCommand;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod.EventBusSubscriber
+public class CommandEventHandler {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    @SubscribeEvent
+    public static void onRegisterCommands(RegisterCommandsEvent event) {
+        HelpCommand.register(event.getDispatcher());
+        LOGGER.info("Registered Command Event Handler");
+    }
+}
---
+++ src/main/java/com/dorpeled/pterodactylcommandsmod/util/Constants.java
@@ -0,0 +1,14 @@
+package com.dorpeled.pterodactylcommandsmod.util;
+
+public class Constants {
+    public static final String HELP_MESSAGE = """
+            Usage:
+            /pterodactyl config setApiKey <apiKey>
+            /pterodactyl config setBaseUrl <baseUrl>
+            /pterodactyl config setServerId <serverId>
+            /pterodactyl schedule_execute <scheduleId>
+            /pterodactyl server_start <serverId>
+            /pterodactyl server_stop <serverId>
+            /pterodactyl server_restart <serverId>
+            """;
+}
